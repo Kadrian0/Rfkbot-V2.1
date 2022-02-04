6a5b88fc08cc13d8f0d46a722cce71532c419ce5
Kadrian0 <96083185+Kadrian0@users.noreply.github.com>
2022-02-04 20:08:30 +0800

    Update vah.js

--- plugins/vah.js
+++ plugins/vah.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 ye = `@${m.sender.split`@`[0]}`
-let info = `Hai Kak ${ye} Nih Sc ini menggunakan SC dari https://github.com/AlyaaXd/Rfkbot-V2 Jan lupa follow yah
+let info = `Hai Kak ${ye} apa Cari apa? Cari Sc? owkaowkaow ngakak aja bro🤣🤣
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
